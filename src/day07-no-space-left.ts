580303653113542ddc77297b6556b8a643ba17fc
Puyan Wei <puyanwei@gmail.com>
2022-12-20 15:46:05 +0000

    trying to iterate bottom up by folder level

--- src/day07-no-space-left.ts
+++ src/day07-no-space-left.ts
@@ -1,14 +1,18 @@
 import { Directory, File } from './types'
-import { writeToJson } from './utilities/writeToJson'
+import { sum, writeToJson } from './utilities'
 
 let count = 0
 
 export function daySevenPartOne(input: string) {
   const directories = resolveDirectoryObject(input)
   const directoreiesSortedByLowestLevels = directories.sort(
-    (a, b) => a.level - b.level
+    (a, b) => b.level - a.level
   )
-  writeToJson(directoreiesSortedByLowestLevels, `directories.json`)
+
+  // stops Jest test overwriting the example JSON file
+  //  if(!input.includes(`14848514 b.txt`)) writeToJson(directoreiesSortedByLowestLevels, `directories.json`)
+
+  const updatedDirectories = updateDirectories(directoreiesSortedByLowestLevels)
 
   // const updatedDirectories = updateDirectoryTotals(directories)
   // const directoriesLessThan100k = updatedDirectories.filter(
@@ -22,109 +26,55 @@ export function daySevenPartOne(input: string) {
   return
 }
 
-// function updateDirectoryTotals(directories: Directory[]) {
-//   const directoryIndexMap = resolveDirectoryIndexMap(directories)
-//   const rootDirectoryIndex = directoryIndexMap['/']
-//   // const numberOfDirectoriesToUpdate = Object.keys(directoryIndexMap).length
-//   const numberOfDirectoriesToUpdate = 65
-//   let newDirectories = directories
-//   for (let index = 0; index < numberOfDirectoriesToUpdate; index++) {
-//     const directoryToUpdate =
-//       findLowestDirectoryUpdateSizeAndReturnItToBeUpdatedElsewhere(
-//         newDirectories,
-//         rootDirectoryIndex
-//       )
-//     // console.log('DIRECTORY TO UPDATE', directoryToUpdate)
-//     // if (index === 64) writeToJson(newDirectories, `directories.json`)
-//     newDirectories = updateDirectories(newDirectories, directoryToUpdate)
-//   }
-//   // writeToJson(newDirectories, `directories.json`)
-//   return newDirectories
-// }
+function updateDirectories(directories: Directory[]) {
+  const highestLevel = directories[0].level
+  const levels = Array.from(Array(highestLevel).keys()).reverse()
+  let resolvedDirectories: Directory[] = directories
 
-// function updateDirectories(
-//   directories: Directory[],
-//   directoryToUpdate: DirectorySize
-// ) {
-//   const updatedDirectories: Directory[] = directories.map((directory) => {
-//     const updatedFiles = directory.files.map((file) => {
-//       const directoryAsFileName = `dir ${directoryToUpdate.directoryName}`
-//       const isMatchedDirectoryToUpdate =
-//         file.fileName === directoryAsFileName && file.fileName.startsWith('dir')
+  // interates per folder level number bottom up
+  for (let index = 0; index < 1; index++) {
+    resolvedDirectories = updateDirectoryTotalsForBottomLevel(
+      resolvedDirectories,
+      levels[index] + 1
+    )
+    updateDirectorySizes(resolvedDirectories, levels[index] + 1)
+  }
+  // console.log('DIR', JSON.stringify(resolvedDirectories[2], undefined, 2))
+  return resolvedDirectories
+}
 
-//       // console.log(
-//       //   'IS MATCHING',
-//       //   isMatchedDirectoryToUpdate,
-//       //   directoryAsFileName,
-//       //   '=>',
-//       //   file.fileName
-//       // )
-//       return isMatchedDirectoryToUpdate
-//         ? { ...file, size: directoryToUpdate.totalSize }
-//         : file
-//     })
-//     const newTotalSize = updatedFiles.reduce((acc, curr) => acc + curr.size, 0)
-//     const updatedDirectory = {
-//       ...directory,
-//       files: updatedFiles,
-//       totalSize: newTotalSize,
-//     }
-//     return updatedDirectory
-//   })
-//   return updatedDirectories
-// }
+interface NewSizeDetails {
+  directoryName: string
+  totalSize: number
+  level: number
+}
 
-// function resolveDirectoryIndexMap(directories: Directory[]) {
-//   const obj: Record<string, number> = {}
-//   directories.forEach((directory, index) => {
-//     return (obj[directory.directoryName] = index)
-//   })
-//   return obj
-// }
+function updateDirectorySizes(
+  directories: Directory[],
+  folderLevel: number
+): Directory[] {
+  let newSizeDetails: NewSizeDetails[] = []
 
-// interface DirectorySize {
-//   directoryName: string
-//   totalSize: number
-// }
+  directories.forEach(({ level, totalSize, directoryName }) => {
+    if (level !== folderLevel) return
+    if (totalSize === 'unknown') return
+    newSizeDetails.push({ directoryName, totalSize, level })
+  })
 
-// function findLowestDirectoryUpdateSizeAndReturnItToBeUpdatedElsewhere(
-//   directories: Directory[],
-//   directoryIndex: number
-// ): DirectorySize {
-//   // console.log(`PARENTS`, directories[directoryIndex].directoryName)
-//   const directoryWithNoSize = directories[directoryIndex].files.find(
-//     (file) => file.size === 0 && file.fileName.startsWith('dir')
-//   )
+  console.log('NEW SIZE OBJ', newSizeDetails)
+  return directories
+}
 
-//   /* Keeps recursively checking for directories without a size in it's children until there isn't any. Then once there is none we can be confident that directory has a file size and no dependent file directories on it, so we update the file size and return it to be updated in all other areas
-//    */
-
-//   if (!!directoryWithNoSize) {
-//     // console.log(
-//     //   `INTERATION NO. ${count} - PWD IN ${directoryWithNoSize?.fileName}`
-//     // )
-//     const name = getAllButLastWord(directoryWithNoSize.fileName, ' ')
-//     const targetDirectoryIndex = resolveDirectoryIndexMap(directories)[name]
-//     count++
-//     return findLowestDirectoryUpdateSizeAndReturnItToBeUpdatedElsewhere(
-//       directories,
-//       targetDirectoryIndex
-//     )
-//   }
-//   const lowestDirectoryFileInFileTree = directories[directoryIndex]
-
-//   const newTotalSize = lowestDirectoryFileInFileTree.files.reduce(
-//     (acc, curr) => acc + curr.size,
-//     0
-//   )
-//   const lowestDirectoryWithTotalSize = {
-//     ...lowestDirectoryFileInFileTree,
-//     totalSize: newTotalSize,
-//   }
-//   count++
-//   // console.log(`LOWEST DIRECTORY`, lowestDirectoryWithTotalSize.directoryName)
-//   return lowestDirectoryWithTotalSize
-// }
+function updateDirectoryTotalsForBottomLevel(
+  directories: Directory[],
+  folderLevel: number
+): Directory[] {
+  return directories.map((directory) => {
+    if (directory.level !== folderLevel) return directory
+    const totalSize = sum(directory.files.map((file) => file.size as number))
+    return { ...directory, totalSize }
+  })
+}
 
 export function resolveDirectoryObject(string: string): Directory[] {
   let resolvedLevel = 0
@@ -136,7 +86,8 @@ export function resolveDirectoryObject(string: string): Directory[] {
       const totalSize = resolveTotalSize(fileNames)
       const files = resolveFileObject(fileNames)
 
-      const level = lines === ' ..\n' ? resolvedLevel-- : resolvedLevel++
+      lines === ' ..\n' ? resolvedLevel-- : resolvedLevel++
+
       return {
         directoryName,
         files,
@@ -146,10 +97,6 @@ export function resolveDirectoryObject(string: string): Directory[] {
     })
     .filter((e) => e.directoryName !== '..')
   return resolvedDirectories
-}
-
-function resolveLevel(string: string): number {
-  return 9
 }
 
 function resolveFileObject(array: string[]): File[] {
@@ -185,13 +132,4 @@ function resolveTotalSize(lines: string[]): number | 'unknown' {
   if (!sizes.length || !sizes) return 'unknown'
   if (sizes.some((e) => e === 'unknown')) return 'unknown'
   return sizes.reduce((acc, curr) => (acc as number) + (curr as number), 0)
-}
-
-function getFirstWord(string: string, splitCondition: string) {
-  return string.split(splitCondition)[0]
-}
-
-function getAllButLastWord(string: string, splitCondition: string) {
-  const [first, ...rest] = string.split(splitCondition)
-  return rest.join()
 }
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -5,3 +5,7 @@ export function writeToJson(data: unknown, fileName: string) {
   let json = JSON.stringify(data, undefined, 2)
   fs.writeFileSync(path.join(`src/jsonFromFS`, fileName), json, 'utf8')
 }
+
+export function sum(array: number[]): number {
+  return array.reduce((prev, current) => (prev += current), 0)
+}
