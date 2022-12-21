9f6632bcc5785b101b172f09f507a900933e70bc
Puyan Wei <puyanwei@gmail.com>
2022-12-21 23:20:28 +0000

    Finished!!! Just KIDDING xD

--- src/day07-no-space-left.ts
+++ src/day07-no-space-left.ts
@@ -5,14 +5,16 @@ let count = 0
 
 export function daySevenPartOne(input: string) {
   const directories = resolveDirectoryObject(input)
-  const directoreiesSortedByLowestLevels = directories.sort(
+  const directoriesSortedByLowestLevels = directories.sort(
     (a, b) => b.level - a.level
   )
 
   // stops Jest test overwriting the example JSON file
-  //  if(!input.includes(`14848514 b.txt`)) writeToJson(directoreiesSortedByLowestLevels, `directories.json`)
+  //  if(!input.includes(`14848514 b.txt`)) writeToJson(directoriesSortedByLowestLevels, `directories.json`)
 
-  const updatedDirectories = updateDirectories(directoreiesSortedByLowestLevels)
+  const updatedDirectories = updateDirectorySizeLowestLevelsUpwards(
+    directoriesSortedByLowestLevels
+  )
 
   // const updatedDirectories = updateDirectoryTotals(directories)
   // const directoriesLessThan100k = updatedDirectories.filter(
@@ -26,53 +28,78 @@ export function daySevenPartOne(input: string) {
   return
 }
 
-function updateDirectories(directories: Directory[]) {
+function updateDirectorySizeLowestLevelsUpwards(directories: Directory[]) {
   const highestLevel = directories[0].level
-  const levels = Array.from(Array(highestLevel).keys()).reverse()
-  let resolvedDirectories: Directory[] = directories
+  const levels = Array.from(Array(highestLevel).keys())
 
+  let resolvedDirectories: Directory[] = directories
   // interates per folder level number bottom up
-  for (let index = 0; index < 1; index++) {
-    resolvedDirectories = updateDirectoryTotalsForBottomLevel(
+
+  for (let index = levels.length - 1; index > 0; index--) {
+    resolvedDirectories = updateDirectorySizeForBottomLevelOnly(
       resolvedDirectories,
-      levels[index] + 1
+      index + 1
     )
-    updateDirectorySizes(resolvedDirectories, levels[index] + 1)
+    // updateDirectoryFolderSizes(resolvedDirectories, levels[index] + 1)
   }
-  // console.log('DIR', JSON.stringify(resolvedDirectories[2], undefined, 2))
+  // console.log('DIR', JSON.stringify(resolvedDirectories, undefined, 2))
   return resolvedDirectories
 }
 
-interface NewSizeDetails {
-  directoryName: string
-  totalSize: number
-  level: number
-}
-
-function updateDirectorySizes(
+function updateDirectoryFolderSizes(
   directories: Directory[],
   folderLevel: number
 ): Directory[] {
-  let newSizeDetails: NewSizeDetails[] = []
+  /*  
+1. Resolve object into "easy-to-work-with" format. An object that is an array of directory objects, which has properties but also a file array. This is done by looking at the data with the terminal commands
+2. Sort object by highest number level (which is the lowest in the tree)
+3. Update sizes of directories in lowest level (because this is guarenteed to not have children that are directories)
 
-  directories.forEach(({ level, totalSize, directoryName }) => {
-    if (level !== folderLevel) return
-    if (totalSize === 'unknown') return
-    newSizeDetails.push({ directoryName, totalSize, level })
-  })
 
-  console.log('NEW SIZE OBJ', newSizeDetails)
+4. Update the tree for those directories' sizes folders (that are in the file array)
+5. Do step 3 (and 4) with the next level... until top level reached
+6. With the object completed, iterate over all directories under 100k
+7. Get answer!
+*/
+
   return directories
 }
 
-function updateDirectoryTotalsForBottomLevel(
+function updateDirectorySizeForBottomLevelOnly(
   directories: Directory[],
   folderLevel: number
 ): Directory[] {
   return directories.map((directory) => {
     if (directory.level !== folderLevel) return directory
-    const totalSize = sum(directory.files.map((file) => file.size as number))
-    return { ...directory, totalSize }
+
+    const totalFilesSize = sum(
+      directory.files
+        .filter((file) => file.type === 'file')
+        .map((file) => file.size as number)
+    )
+
+    const totalNestedDirectoriesSize = sum(
+      directory.files
+        .filter((file) => file.type === 'folder')
+        .map((folder) => {
+          const resolvedDirectory = directories.find((directory) => {
+            // console.log(
+            //   'MATCHES',
+            //   directory.directoryName,
+            //   folder.fileName,
+            //   `dir ${directory.directoryName}` === folder.fileName
+            // )
+            return (
+              `dir ${directory.directoryName}` === folder.fileName &&
+              directory.level === folderLevel + 1
+            )
+          }) as Directory
+          return resolvedDirectory.totalSize as number
+        })
+    )
+    directory.totalSize = totalNestedDirectoriesSize
+    console.log(JSON.stringify(directory, undefined, 2))
+    return directory
   })
 }
 
@@ -110,7 +137,6 @@ function resolveFileObject(array: string[]): File[] {
       const fileName = fileInfo.startsWith('dir') ? fileInfo : rest.join()
       const size = fileInfo.startsWith('dir') ? 'unknown' : parseInt(firstWord)
       const type = fileInfo.startsWith('dir') ? 'folder' : 'file'
-
       return {
         fileName,
         size,
